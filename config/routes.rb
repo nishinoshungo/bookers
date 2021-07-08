ac91b5d91e7b3115817d801fe62fe5f065f9ebe0
nishinoshungo <nishishun50@gmail.com>
2021-07-08 05:02:34 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get 'books', to: 'books#index'
+  post 'books', to: 'books#create'
+  get 'books/:id', to: 'books#show', as: 'book'
+  get 'books/:id/edit', to: 'books#edit', as: 'edit_book'
+  patch 'books/:id', to: 'books#update', as: 'update_book'
+  delete 'books/:id', to: 'books#destroy', as: 'destroy_book'
 end
